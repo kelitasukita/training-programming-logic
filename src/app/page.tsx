42de0d21ef98c8222a7f52524032c6e46aa78505
Kelita Lamar <kelitasukita@gmail.com>
2023-05-03 22:49:59 +0200

    Set League-Spartan as a project font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,14 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className='flex justify-center items-center mt-8'>
-      <div className='bg-teal-600 rounded-lg w-3/5 px-7 py-6'>
-        <h1 className='text-zinc-300 text-xl font-semibold text-center'>
-          Click on the buttons below to access their respective challenges!
-        </h1>
-      </div>
-
+    <main>
+      <article>
+        <div className='bg-teal-600 px-7 py-6'>
+          <h1 className='text-zinc-300 text-xl font-semibold text-center'>
+            Click on the buttons below to access their respective challenges!
+          </h1>
+        </div>
+      </article>
     </main>
   )
 }
