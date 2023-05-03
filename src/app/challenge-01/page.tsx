bcc214499eadd486ddaa71d1c797f430384d1180
Kelita Lamar <kelitasukita@gmail.com>
2023-05-04 01:39:02 +0200

    Set a challenge question and options

--- src/app/challenge-01/page.tsx
+++ src/app/challenge-01/page.tsx
@@ -13,6 +13,19 @@ export default function Challenge01() {
           <p className='text-zinc-500 mt-6'>Faça um programa para ler a quantidade máxima, mínima e atual de um estoque. Calcule a quantidade média M = (QTDMax + QTDMin)/2. Se a quantidade atual for maior ou igual a quantidade maior, retornar a resposta: "Não efetuar compra", se estiver abaixo retorne: "Efetuar compra".</p>
         </article>
 
+        <article className='w-4/5 mx-auto mt-12 text-zinc-500 flex flex-col'>
+          <label className='mb-2'>Informe a quantidade máxima do estoque:</label>
+          <input type="number" className='h-8 rounded-lg w-1/2' />
+          <label className='mb-2 mt-4'>Informe a quantidade mínima do estoque:</label>
+          <input type="number" className='h-8 rounded-lg w-1/2' />
+          <label className='mb-2 mt-4'>Informe a quantidade atual do estoque:</label>
+          <input type="number" className='h-8 rounded-lg w-1/2' />
+        </article>
+
+        <div className='w-4/5 mx-auto mt-12'>
+          <p className='text-teal-600 text-lg font-bold'></p>
+        </div>
+
       </main >
     </>
   )
