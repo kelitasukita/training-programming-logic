0e6204cd8a63b6bd743475e4416e09d3ac9e53c8
Kelita Lamar <kelitasukita@gmail.com>
2023-05-05 16:59:29 +0200

    Create and resolve challenge 01

--- src/app/challenge-01/page.tsx
+++ src/app/challenge-01/page.tsx
@@ -1,6 +1,26 @@
 /* eslint-disable react/no-unescaped-entities */
+'use client';
+
+import { useState } from 'react';
+
 import ButtonLink from '../components/ButtonLink';
+
 export default function Challenge01() {
+  const [maxima, setMaxima] = useState<number>(0);
+  const [minima, setMinima] = useState<number>(0);
+  const [atual, setAtual] = useState<number>(0);
+
+  const handleMedia = () => {
+    const media = (maxima + minima) / 2;
+    console.log(media, maxima, minima);
+
+    if (atual >= media) {
+      return "NÃO efetuar compra"
+    } else {
+      return "EFETUAR compra"
+    }
+  };
+
   return (
     <>
       <main>
@@ -10,21 +30,25 @@ export default function Challenge01() {
 
         <article className='w-4/5 mx-auto mt-12'>
           <h2 className='text-teal-600 text-lg font-bold'>Exercício 01:</h2>
-          <p className='text-zinc-500 mt-6'>Faça um programa para ler a quantidade máxima, mínima e atual de um estoque. Calcule a quantidade média M = (QTDMax + QTDMin)/2. Se a quantidade atual for maior ou igual a quantidade maior, retornar a resposta: "Não efetuar compra", se estiver abaixo retorne: "Efetuar compra".</p>
+          <p className='text-zinc-500 mt-6'>Faça um programa para ler a quantidade máxima, mínima e atual de um estoque. Calcule a quantidade média M = (QTDMax + QTDMin)/2. Se a quantidade atual for maior ou igual a quantidade media, retornar a resposta: "Não efetuar compra", se estiver abaixo retorne: "Efetuar compra".</p>
         </article>
 
         <article className='w-4/5 mx-auto mt-12 text-zinc-500 flex flex-col'>
           <label className='mb-2'>Informe a quantidade máxima do estoque:</label>
-          <input type="number" className='h-8 rounded-lg w-1/2 px-4' />
+          <input type="number" className='h-8 rounded-lg w-1/2 px-4' onChange={(e) => setMaxima(parseInt(e.target.value))} />
+
           <label className='mb-2 mt-4'>Informe a quantidade mínima do estoque:</label>
-          <input type="number" className='h-8 rounded-lg w-1/2 px-4' />
+          <input type="number" className='h-8 rounded-lg w-1/2 px-4' onChange={(e) => setMinima(parseInt(e.target.value))} />
+
           <label className='mb-2 mt-4'>Informe a quantidade atual do estoque:</label>
-          <input type="number" className='h-8 rounded-lg w-1/2 px-4' />
+          <input type="number" className='h-8 rounded-lg w-1/2 px-4' onChange={(e) => setAtual(parseInt(e.target.value))} />
         </article>
 
         <div className='w-4/5 mx-auto mt-12'>
-          <p className='text-teal-600 text-lg font-bold'></p>
+          <p className='text-teal-600 text-lg font-bold'>{handleMedia()}</p>
         </div>
+
+
 
       </main >
     </>
